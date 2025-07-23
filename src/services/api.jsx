c39e874a4c065317ef1ca7ca6c8f5b8edb749060
chepe020 <joseajcu11@gmail.com>
2025-07-23 13:22:40 -0600

    link diferente

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiBanc = axios.create({
-    baseURL: "http://localhost:8080/BancoSystem/v1",
+    baseURL: "banc-production.up.railway.app/BancoSystem/v1",
     timeout: 5000,
     headers: { "Cache-Control": "no-cache, no-store, must-revalidate" }
 })
