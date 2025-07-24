3102919e711c588c17591576153cae9c92237b7a
chepe020 <joseajcu11@gmail.com>
2025-07-24 14:41:08 -0600

    Solución errror

--- src/pages/Deposit/DepositPage.jsx
+++ src/pages/Deposit/DepositPage.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from "../../components/Navbar/Navbar"
 import {Footer} from "../../components/Footer/Footer"
-import { Deposit } from "../../components/deposit/Deposit"
+import { Deposit } from "../../components/Deposit/Deposit"
 
 export const DepositPage = () => {
     return(
